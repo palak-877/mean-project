56442dcb24fd84d12531114819f4ed2769ee7b5e
Manya <manya9605@gmail.com>
2026-04-18 15:52:03 +0530

    add Wisdom schema for storing Bhagavad Gita teachings

---
+++ backend/models/Wisdom.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const wisdomSchema = new mongoose.Schema({
+  keyword: String,
+  teaching: String
+});
+
+module.exports = mongoose.model('Wisdom', wisdomSchema);
